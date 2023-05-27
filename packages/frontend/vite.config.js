78e653a609f4c8b831fe11a537f1f966ebc62275
Christoph Thiede <christoph.thiede@student.hpi.de>
2023-05-27 03:43:18 +0200

    fix package structure for builds

--- packages/frontend/vite.config.js
+++ packages/frontend/vite.config.js
@@ -2,7 +2,13 @@ import { defineConfig } from 'vite'
 import customCjsToEsmPlugin from './customCjsToEsmPlugin'
 
 export default defineConfig({
+  root: 'src',
   base: '', // relative path to support http server from parent folder
+
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
 
   esbuild: {
     supported: {
